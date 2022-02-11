3f5df008e51718d6eb52abbd51fae42289e9d2c6
terryszhou <terryszhou@gmail.com>
2022-02-11 08:53:41 -0600

    Task component added, fontawesome cdn imported

---
+++ src/components/Task.vue
@@ -0,0 +1,46 @@
+<template>
+  <div v-bind:class="[task.reminder && 'reminder', 'task']">
+    <h3>
+      {{ task.text }} 
+      <i v-on:click="onDelete(task.id)" class="fas fa-times" />
+    </h3>
+    <p>{{ task.day }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Task",
+    props: {
+      task: Object,
+    },
+    methods: {
+      onDelete(id) {
+        console.log(id)
+      }
+    }
+  }
+</script>
+
+<style scope>
+  .fas {
+    color: red;
+  }
+
+  .task {
+    background: #f4f4f4;
+    margin: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+  }
+
+  .task.reminder {
+    border-left: 5px solid green;
+  }
+
+  .task h3 {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+</style>
--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,14 +1,19 @@
 <template>
   <div v-for="task in tasks" v-bind:key="task.id">
-    <h3>{{ task.text }}</h3>
+    <Task v-bind:task="task"/>
   </div>
 </template>
 
 <script>
+  import Task from "./Task.vue"
+
   export default {
     name: "Tasks",
     props: {
       tasks: Array,
+    },
+    components: {
+      Task,
     },
   }
 </script>
