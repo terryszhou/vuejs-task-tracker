f9e90e2c46a88d82a2659e83e30d505b77c9ed70
terryszhou <terryszhou@gmail.com>
2022-02-11 10:02:56 -0600

    json simulated server added; addTask function refactored

--- src/App.vue
+++ src/App.vue
@@ -36,8 +36,18 @@
       toggleAddTask() {
         this.showAddTask = !this.showAddTask
       },
-      addTask(task) {
-        this.tasks = [...this.tasks, task]
+      async addTask(task) {
+        const res = await fetch("api/tasks", {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(task)
+        })
+
+        const data = await res.json()
+
+        this.tasks = [...this.tasks, data]
       },
       deleteTask(id) {
         if (confirm("Are you sure?")) {
@@ -50,29 +60,20 @@
             ? {...task, reminder: !task.reminder} 
             : task
         ))
+      },
+      async fetchTasks() {
+        const res = await fetch("api/tasks")
+        const data = await res.json()
+        return data
+      },
+      async fetchTask(id) {
+        const res = await fetch(`api/tasks/${id}`)
+        const data = await res.json()
+        return data
       }
     },
-    created() {
-      this.tasks = [
-        {
-          id: 1,
-          text: "Doctor's Appointment",
-          day: "March 1st, 2:30pm",
-          reminder: true,
-        },
-        {
-          id: 2,
-          text: "School Meeting",
-          day: "March 3rd, 1:30pm",
-          reminder: true,
-        },
-        {
-          id: 3,
-          text: "Grocery Shopping",
-          day: "March 3rd, 11:00am",
-          reminder: false,
-        },
-      ]
+    async created() {
+      this.tasks = await this.fetchTasks()
     }
   }
 </script>
